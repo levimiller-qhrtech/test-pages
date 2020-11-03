4be1aec9fd7ddf4f00291f5cf04010f2eb26aec6
Levi Miller <levi.miller@qhrtech.com>
2020-11-03 11:35:50 -0800

    add new-app schematic

---
+++ tools/schematics/new-app/index.ts
@@ -0,0 +1,31 @@
+import { chain, externalSchematic, Rule } from "@angular-devkit/schematics";
+import { SchematicOptions } from './schema';
+import {updateWorkspaceInTree} from '@nrwl/workspace';
+
+function generateLibrary(schema: SchematicOptions): Rule {
+  return externalSchematic(`@nrwl/${schema.plugin}`, "app", {
+    name: schema.name
+  });
+}
+
+function addReleaseRunCommand(schema: SchematicOptions): Rule {
+  return updateWorkspaceInTree((workspace) => {
+    workspace.projects[schema.name].architect['release'] = {
+      builder: '@nrwl/workspace:run-commands',
+      options: {
+        commands: [
+          `nx build --project=${schema.name} --prod --output-path docs/${schema.name} --base-href /test-pages/${schema.name}`,
+        ],
+        parallel: false
+      }
+    };
+    return workspace;
+  });
+}
+
+export default function (schema: SchematicOptions): Rule {
+  return chain([
+    generateLibrary(schema),
+    addReleaseRunCommand(schema)
+  ]);
+}
